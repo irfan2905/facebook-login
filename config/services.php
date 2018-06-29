0d8a20b5357da04aa3c14460bc86fc8dc9133d97
irfan2905 <irfan.naufal16@gmail.com>
2018-06-29 22:19:05 +0700

    Add existing file

---
+++ config/services.php
@@ -0,0 +1,39 @@
+<?php
+
+return [
+
+    /*
+      |--------------------------------------------------------------------------
+      | Third Party Services
+      |--------------------------------------------------------------------------
+      |
+      | This file is for storing the credentials for third party services such
+      | as Stripe, Mailgun, SparkPost and others. This file provides a sane
+      | default location for this type of information, allowing packages
+      | to have a conventional place to find your various credentials.
+      |
+     */
+
+    'mailgun' => [
+        'domain' => env('MAILGUN_DOMAIN'),
+        'secret' => env('MAILGUN_SECRET'),
+    ],
+    'ses' => [
+        'key' => env('SES_KEY'),
+        'secret' => env('SES_SECRET'),
+        'region' => env('SES_REGION', 'us-east-1'),
+    ],
+    'sparkpost' => [
+        'secret' => env('SPARKPOST_SECRET'),
+    ],
+    'stripe' => [
+        'model' => App\User::class,
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+    ],
+    'facebook' => [
+        'client_id' => '474144466364351',
+        'client_secret' => '1a1439d998e10cd72ad0bb5dfd42bc71',
+        'redirect' => 'http://localhost:8000/callback',
+    ],
+];
